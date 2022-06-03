fa86b2b798c24caaa5516d190acfe821cda94eff
Paulk <Paulk@LAPTOP-7M25O7VI>
2022-06-03 19:19:50 -0400

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketApp.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Biography { get; set; }
+    }
+}
