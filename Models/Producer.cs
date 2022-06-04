0df78fe386884a35ebf6a294473139a24d5b4d26
Paul-kav <paulkavuma82@gmail.com>
2022-06-04 06:39:14 -0400

    Added the producer model.

---
+++ Models/Producer.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketApp.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Description { get; set; }
+    }
+}
