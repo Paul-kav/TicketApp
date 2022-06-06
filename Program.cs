38cd5d884d0bc0fc95297aa8c8ff925904f44c86
Paul-kav <paulkavuma82@gmail.com>
2022-06-06 12:41:38 -0400

    Added the connection string and configured sql

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TicketApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnectionString")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
