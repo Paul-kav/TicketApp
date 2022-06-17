0d1fc6520e34a468be2a8a233cbfb18768efd6df
Paul-kav <paulkavuma82@gmail.com>
2022-06-17 12:08:20 -0400

    Added the actors interface

---
+++ Data/Services/ActorsInterface.cs
@@ -0,0 +1,16 @@
+﻿using TicketApp.Models;
+
+namespace TicketApp.Data.Services
+{
+    public interface Actors
+    {
+        IEnumerable<Actors> GetAll();
+        Actor GetById();
+        void Update(int id, Actor newActor );
+        Actor Add(int id);
+        void Delete(int id);
+
+
+
+    }
+}
