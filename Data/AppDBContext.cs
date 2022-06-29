8ec58ff62bb31da1aad0ea58917e29c8e42118b8
Paul-kav <paulkavuma82@gmail.com>
2022-06-28 21:29:04 -0400

    Added the producers table to the db context  file

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -28,6 +28,7 @@ namespace TicketApp.Data
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
 
 
     }
