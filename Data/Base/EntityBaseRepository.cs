a6a8440861d0f39f0c7bbe375b977a6a3fb144dc
Paul-kav <paulkavuma82@gmail.com>
2022-07-05 14:50:36 -0400

    Added the getallasync and getbyidasync into the entitybase repository

--- Data/Base/EntityBaseRepository.cs
+++ Data/Base/EntityBaseRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace TicketApp.Data.Base
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketApp.Data.Base
 {
+    
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+        //inject the app dbcontext file to be able to work with the database
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public Task AddAsync(T entity)
         {
             throw new NotImplementedException();
@@ -12,19 +22,23 @@
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var allresults = await _context.Set<T>().ToListAsync();
+            return allresults;
+
         }
 
-        public Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            return result;
         }
 
         public Task<T> UpdateAsync(int id, T entity)
         {
             throw new NotImplementedException();
         }
+        
     }
 }
