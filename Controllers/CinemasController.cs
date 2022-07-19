c6d7be377a8008a735e31cf7231d5a726ae45347
Paul-kav <paulkavuma82@gmail.com>
2022-07-19 14:36:02 -0400

    Added the delete action result and view

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TicketApp.Data;
 using TicketApp.Data.Services;
+using TicketApp.Models;
 
 namespace TicketApp.Controllers
 {
@@ -27,5 +28,54 @@ namespace TicketApp.Controllers
             return View(cinemaDetails);
         }
 
+        //GET:cinemas/create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName, Description")] Cinema cinema)//we bind all the properties we need for the cinema
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cinema);
+            }
+            await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        //GET: cinema/edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("id, FullName, Description")] Cinema cinema)
+        {
+            if(!ModelState.IsValid) return View(cinema);
+            if (cinema == null) return View("NotFound");
+            await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: cinema/delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteComfirmed(int id)
+        {
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
